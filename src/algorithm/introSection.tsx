861b973c3ac06a9e8e4b0ccfc35ab6ea592f1b86
razvan404 <razvan.petec@sectorlabs.ro>
2025-05-30 13:57:26 +0300

    Add introductory section for algorithm chapter

--- src/algorithm/introSection.tsx
+++ src/algorithm/introSection.tsx
@@ -5,11 +5,32 @@ import { IntroSection } from '@/core';
 import { Sections, getTitle } from './sections';
 
 function AlgorithmIntroSection() {
-    const description = React.useMemo(() => <p>example</p>, []);
+    const description = React.useMemo(
+        () => (
+            <>
+                <p>
+                    This chapter brings everything together by presenting the
+                    full Genetic Algorithm in action. You’ll be able to interact
+                    with its components, observe how populations evolve over
+                    generations, and see how crossover, mutation, and selection
+                    shape the search for optimal solutions.
+                </p>
+                <p>
+                    It begins by introducing key{' '}
+                    <strong>hyperparameters</strong> that influence the
+                    algorithm’s behavior, followed by a{' '}
+                    <strong>step-by-step visualization</strong> of the
+                    evolutionary process as it solves the{' '}
+                    <strong>Traveling Salesman Problem</strong>.
+                </p>
+            </>
+        ),
+        [],
+    );
 
     return (
         <IntroSection
-            chapter={3}
+            chapter={4}
             title={getTitle(Sections.INTRO)}
             description={description}
             subsections={Object.values(Sections)
